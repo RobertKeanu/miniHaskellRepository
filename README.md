ebe204187e64cc4444a2cdb9fe2ad8f89d8e91f8
Roșianu Robert <94984223+RobertKeanu@users.noreply.github.com>
2023-05-08 14:16:09 +0300

    sapt11

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# miniHaskellRepository
+# miniHaskell
--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,38 @@
+
 module Main where
 
 import System.IO
-import System.Console.Isocline
 
+import Lab2
 import Exp
 import Parsing
-import Printing
-import Eval
 import Sugar
+import Eval
+import Printing
 import REPLCommand
+import Program
 
 main :: IO ()
-main = do
-    setHistory "history.txt" 200
-    input <- readline "miniHaskell"
-    case parse replCommand "<input>" input of:
-        Left err -> print err >> main
-        Right cmd -> case cmd of:
-                        Quit -> return ()
-                        Load _ -> putStrLn("Not implemented ") >> main
-                        Eval s -> parse exprParser "<input>" l of
-                            Left err -> print err >> main
-                            Right c -> putStrLn ( showExp c) >> main
+main = execute empty
+  
+execute :: Environment -> IO ()
+execute env
+  = do
+    putStr "miniHaskell> "
+    hFlush stdout
+    s <- getLine
+    case parseFirst replCommand s of
+          Nothing -> putStrLn "Cannot parse command" >> execute env
+          Just Quit -> return ()
+          Just (Load path) ->
+            case parseFirst string path of
+              Nothing -> putStrLn "Invalid file path" >> execute env 
+              Just p -> parseFromFile program p >>= \list -> programEnv list  >>= \ev -> execute ev
+          Just (Eval es) ->
+            case parseFirst exprParser es of
+              Nothing -> putStrLn "Error: cannot parse expression" >> execute env
+              Just e ->
+                let simpleE = desugarExp e
+                    simpleE' = normalizeEnv env simpleE
+                    e' = sugarExp simpleE'
+                 in putStrLn (showExp e') >> execute env
